8dd3489b03b6ddcdc75a6480753ce290f98ace93
LiuZeGuang <lzg569472734@163.com>
2018-12-20 20:43:46 +0800

    第一次提交

---
+++ git_demo01/src/main/java/User.java
@@ -0,0 +1,27 @@
+/**
+ * @ClassName User
+ * @Desctiption TODO
+ * @Author 56947
+ * @Date 2018/12/19 21:45
+ * @Version 1.0
+ **/
+public class User {
+    private String name;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
